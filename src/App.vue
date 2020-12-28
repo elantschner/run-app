fa0118bb53bd042c5de2860097c72bd21ecbb033
LOEX\elantschner <elantschner@live.com>
2020-12-28 18:06:12 +0100

    add github-url

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/elantschner/run-app"
         target="_blank"
         text
       >
